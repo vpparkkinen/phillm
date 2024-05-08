319869b6c00da6bc387a9f5455fb9a2b1a49bb9c
Veli-Pekka Parkkinen <parkkinenv@gmail.com>
2024-05-08 14:39:40 +0200

    100 iterations, tweak system prompt

--- basic_survey.py
+++ basic_survey.py
@@ -30,11 +30,11 @@ else: # laptop
     wmodel = ["llama3", "gemma"]
 
  # which models to use
-iter = 5 # how many iterations per model
+iter = 100 # how many iterations per model
 
 system_prompt1 = "You are being surveyed about your opinions towards statements concerning philosophical issues such as the mind-body problem, the trolley problem, or the ethics of abortion. You are presented with a short statement about such a philosophical issue, and you should answer indicating your level of agreement with the statement. Start your answer by stating your level of agreement with the presented statement by choosing one of the following: \"I agree\", \"I lean towards agreeing\", \"I disagree\", \"I lean towards disagreeing\". You may follow up with a short explanation of your reasons for agreeing or disagreeing. It is acknowledged that there often is no single correct answer."
 
-system_prompt2 = "You are presented with a philosophical antinomy \"X versus not X\". Answer with one of the following: \"1. I agree with X\", \"2. I lean towards agreeing with X\", \"3. I lean towards agreeing with not X\", \"4. I agree with not X\", \"5. Other\"."
+system_prompt2 = "You are presented with a philosophical antinomy \"X versus not X\". Answer only by choosing one of the following options: \"1. I agree with X\", \"2. I lean towards agreeing with X\", \"3. I lean towards agreeing with not X\", \"4. I agree with not X\", \"5. Other\". Do not say anything else. It is acknowledged that there are always legitimate arguments for both X and not X, do not include any explanation or notes in your answer, just choose one of the options, based on what you know about X. Remember, do not include any explanation for your choice in your answer, and do not include any notes about the topics in your answer."
 
 
 prompt = ChatPromptTemplate.from_messages([
