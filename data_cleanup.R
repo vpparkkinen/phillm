d010e8503fe51bf261588346ebe820e884566f92
Veli-Pekka Parkkinen <parkkinenv@gmail.com>
2024-08-12 08:50:25 +0200

    cleanup

--- data_cleanup.R
+++ data_cleanup.R
@@ -7,12 +7,12 @@ if(is.na(Sys.getenv("RSTUDIO", unset = NA))){
 }
 
 library(data.table)
-ll <- read.csv2("mem_afirst04082024-00h42m.csv")
+ll <- read.csv2("mem_bfirst06082024-20h38m.csv")
 ll <- as.data.table(ll)
 alabel <- sapply(1:27, \(x) rep(x,2), simplify = FALSE) |> unlist()
 rr <- nrow(ll) / length(alabel)
 ll[,Question := rep(alabel, rr)]
-ll[,Antinomy := rep(c("P","N"), nrow(ll)/2)]
+ll[,Antinomy := rep(c("N","P"), nrow(ll)/2)]
 ll[,Presentation_Order := rep(c(1,2), nrow(ll)/2)]
 ll[,A := gsub("^ *", "", ll$A)]
 
@@ -22,5 +22,17 @@ ll <- ll[legit_a_idx,]
 
 ll[,A := regmatches(ll$A, regexpr(legit_a, ll$A))]
 
-write.csv2(ll, "prompt3.1_Pfirst_cleaned.csv")
-     
+write.csv2(ll, "prompt3.1_Nfirst_cleaned.csv")
+
+### merge data sets to create "no conversational memory" data
+
+pfirst <- as.data.table(read.csv2("prompt3.1_Pfirst_cleaned.csv"))
+pfirst <- pfirst[Antinomy == "P",]
+table(pfirst$Antinomy)
+
+nfirst <- as.data.table(read.csv2("prompt3.1_Nfirst_cleaned.csv"))
+nfirst <- nfirst[Antinomy == "N",]
+sum(nfirst$Presentation_Order)
+
+nomem <- rbind(pfirst, nfirst)
+write.csv2(nomem, "prompt31_no_memory.csv")
